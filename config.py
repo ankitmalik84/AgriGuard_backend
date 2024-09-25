d4779515d02bbde0372df078095c8db839f15f64
Ankit Malik <ankitmalik844903@gmail.com>
2024-09-25 23:23:47 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,13 +1,20 @@
-class DevelopmentConfig():
+import os
+
+class Config:
+    """Base configuration."""
+    DEBUG = False
+    TESTING = False
+    SECRET_KEY = os.environ.get('SECRET_KEY', 'your-default-secret-key')  # Set a default or get from env
+
+class DevelopmentConfig(Config):
+    """Development configuration."""
     DEBUG = True
 
+class ProductionConfig(Config):
+    """Production configuration."""
+    DEBUG = False
+
 config = {
-    'development' : DevelopmentConfig
+    'development': DevelopmentConfig,
+    'production': ProductionConfig
 }
-
-
-    
-
-    
-    
-    
